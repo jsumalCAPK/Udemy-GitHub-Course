0e90f3b363aa722486ea6f3015c2a0feec826d25
Jasjot Sumal <jsumal@capk.org>
2026-08-13 10:55:52 -0700

    added multiply_nums function to calculator.py and updated main

--- utils/calculator.py
+++ utils/calculator.py
@@ -2,12 +2,17 @@ def add_nums(a, b):
   return a+b
 
 
-def substract_nums(a, b):
+def subtract_nums(a, b):
   return a-b
 
 
 def divide_nums(a, b):
   return a/b
+
+
+def multiply_nums(a, b):
+  return a*b
+
 
 def power(a, b):
   return a**b
