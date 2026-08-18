9d7d42209459af6b12b4ec5bcbd846fd78ba92c5
Jasjot Sumal <jsumal@capk.org>
2026-08-18 16:50:34 -0700

    updated main.py power function inputs

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
   print("Subtraction: ", subtract_nums(5, 3))
   print("Multiplication: ", multiply_nums(3, 4))
   print("Division: ", divide_nums(5, 3))
-  print("Power: ", power(2, 3))
+  print("Power: ", power(3, 4))
