95c47de48bf91078648af70724eb0100c2956c32
Jasjot Sumal <jsumal@capk.org>
2026-08-18 14:36:40 -0700

    Added labels to prints in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from utils.calculator import add_nums, subtract_nums, divide_nums, multiply_nums, power
 
 if __name__ == "__main__":
-  print(add_nums(1, 3))
-  print(subtract_nums(5, 3))
-  print(divide_nums(5, 3))
-  print(multiply_nums(3, 4))
-  print(power(2, 3))
+  print("Addition: ", add_nums(1, 3))
+  print("Subtraction: ", subtract_nums(5, 3))
+  print("Multiplication: ", multiply_nums(3, 4))
+  print("Division: ", divide_nums(5, 3))
+  print("Power: ", power(2, 3))
